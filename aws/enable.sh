ba70bd1cddbd67dc7a819ec12da68083f3669fb4
Shane Breatnach <shane.breatnach@gmail.com>
2017-03-17 11:20:05 +0000

    WIP

--- aws/enable.sh
+++ aws/enable.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-REGION=${1:-eu-west-2}
-USER_DATA=file://user_data.sh
+TEMP_BUCKET_NAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+USER_DATA_EXTRA_FILE=${1}
+REGION=${2:-eu-west-2}
+USER_DATA=user_data.sh
 KEY_NAME=vpn_key
 
+aws s3 mb ${TEMP_BUCKET_NAME}
+aws s3 cp ${USER_DATA_EXTRA_FILE} s3://${TEMP_BUCKET_NAME}/extra_data.tar.gz
 GROUP_ID=`aws ec2 describe-security-groups --region ${REGION} --query 'SecurityGroups[0].GroupId'`
 SUBNET_ID=`aws ec2 describe-subnets --region ${REGION} --query 'Subnets[0].SubnetId'`
 KEY_DATA=`aws ec2 create-key-pair --key-name ${KEY_NAME} --region ${REGION} --query 'KeyMaterial'`
@@ -11,10 +15,12 @@ KEY_DATA=`aws ec2 create-key-pair --key-name ${KEY_NAME} --region ${REGION} --qu
 # TODO: list AMIs by region looking for specific AMI name
 AMI_ID=
 
-INSTANCE_DATA=`aws ec2 run-instances --image-id ${AMI_ID} --count 1 --instance-type t2.nano --key-name ${KEY_NAME} --user-data ${USER_DATA} --security-group-ids ${GROUP_ID} --subnet-id ${SUBNET_ID} --query 'Instances[0].{instance_id: InstanceId, host: PublicDnsName}' --region ${REGION}`
+sed \'s/%%VPN_BUCKET%%/${TEMP_BUCKET_NAME}/g\' ${USER_DATA}.tpl > ${USER_DATA}
+INSTANCE_DATA=`aws ec2 run-instances --image-id ${AMI_ID} --count 1 --instance-type t2.nano --key-name ${KEY_NAME} --user-data file://${USER_DATA} --security-group-ids ${GROUP_ID} --subnet-id ${SUBNET_ID} --query 'Instances[0].{instance_id: InstanceId, host: PublicDnsName}' --region ${REGION}`
 INSTANCE_ID=
 
 # TODO: wait for shutdown signal
 
+aws s3 rb --force ${TEMP_BUCKET_NAME}
 aws ec2 terminate-instances --instance-ids ${INSTANCE_ID} --region ${REGION}
 aws ec2 delete-key-pair --key-name ${KEY_NAME} --region ${REGION}
